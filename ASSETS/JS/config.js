f05ad96035b2cf58afd72bda6397425cd0009554
variored <varioredip@gmail.com>
2020-06-15 00:34:42 +0700

    add site group slider

---
+++ ASSETS/JS/config.js
@@ -0,0 +1,24 @@
+var COUNT_LANDING           = 2;
+var COUNT_BLOG              = 0;
+var COUNT_SHOP              = 0;
+var COUNT_APP               = 0;
+
+var LANDING_HREFS = [
+                            "https://variored.github.io/site1/",
+                            "https://variored.github.io/site2/"
+];
+var BLOG_HREFS = [
+                            //"https://variored.github.io/site1/",
+                            //"https://variored.github.io/site2/"
+];
+var SHOP_HREFS = [
+                            //"https://variored.github.io/site1/",
+                            //"https://variored.github.io/site2/"
+];
+var APP_HREFS = [
+                            //"https://variored.github.io/site1/",
+                            //"https://variored.github.io/site2/"
+];
+
+//33% in browser
+//150x170

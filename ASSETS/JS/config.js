9721a3c7ef5f9edcdbcc827e5ede5f220f77985b
variowhite <variowhite@outlook.com>
2020-07-21 11:48:05 +0700

    rename sites to TemplateLanding

--- ASSETS/JS/config.js
+++ ASSETS/JS/config.js
@@ -1,42 +1,42 @@
-var COUNT_LANDING           = 3;
-var COUNT_BLOG              = 0;
-var COUNT_SHOP              = 0;
-var COUNT_APP               = 0;
+var COUNT_LANDING = 3;
+var COUNT_BLOG = 0;
+var COUNT_SHOP = 0;
+var COUNT_APP = 0;
 
 var LANDING_TITLES = [
-                            "PREMIUM BUSINES TEMPLATE",
-                            "MOBILE APPLICATION LANDING TEMPLATE",
-                            "SARAJEVO"
+    "PREMIUM BUSINES TEMPLATE",
+    "MOBILE APPLICATION LANDING TEMPLATE",
+    "SARAJEVO"
 ];
 var BLOG_TITLES = [
-                            //"PREMIUM_BUSINES_TEMPLATE",
-                            //"MOBILE APPLICATION LANDING TEMPLATE"
+    //"PREMIUM_BUSINES_TEMPLATE",
+    //"MOBILE APPLICATION LANDING TEMPLATE"
 ];
 var SHOP_TITLES = [
-                            //"PREMIUM_BUSINES_TEMPLATE",
-                            //"MOBILE APPLICATION LANDING TEMPLATE"
+    //"PREMIUM_BUSINES_TEMPLATE",
+    //"MOBILE APPLICATION LANDING TEMPLATE"
 ];
 var APP_TITLES = [
-                            //"PREMIUM_BUSINES_TEMPLATE",
-                            //"MOBILE APPLICATION LANDING TEMPLATE"
+    //"PREMIUM_BUSINES_TEMPLATE",
+    //"MOBILE APPLICATION LANDING TEMPLATE"
 ];
 
 var LANDING_HREFS = [
-                            "https://variowhite.github.io/site1/",
-                            "https://variowhite.github.io/site2/",
-                            "https://variowhite.github.io/site3/"
+    "https://variowhite.github.io/templateLanding1/",
+    "https://variowhite.github.io/templateLanding2/",
+    "https://variowhite.github.io/templateLanding3/"
 ];
 var BLOG_HREFS = [
-                            //"https://variowhite.github.io/site1/",
-                            //"https://variowhite.github.io/site2/"
+    //"https://variowhite.github.io/site1/",
+    //"https://variowhite.github.io/site2/"
 ];
 var SHOP_HREFS = [
-                            //"https://variowhite.github.io/site1/",
-                            //"https://variowhite.github.io/site2/"
+    //"https://variowhite.github.io/site1/",
+    //"https://variowhite.github.io/site2/"
 ];
 var APP_HREFS = [
-                            //"https://variowhite.github.io/site1/",
-                            //"https://variowhite.github.io/site2/"
+    //"https://variowhite.github.io/site1/",
+    //"https://variowhite.github.io/site2/"
 ];
 
 //33% in browser
